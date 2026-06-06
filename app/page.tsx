7750fd6ec5047e3f89fef7136dde96e8ffebf0f1
fawasayoade284-design <fawasayoade284@gmail.com>
2026-06-06 08:38:19 +0100

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-async function getData() {
-  const res = await fetch("https://opensheet.elk.sh/1qwtUjm1ghhxuBK_urQZntHKaKKbH8eAeOT6m23__W0Y/Sheet1", {
-    cache: "no-store"
-  });
-
-  return res.json();
-}
-
 export default async function Home() {
-  const data = await getData();
+  let data = [];
+
+  try {
+    const res = await fetch(
+      "https://opensheet.elk.sh/1qwtUjm1ghhxuBK_urQZntHKaKKbH8eAeOT6m23__W0Y/Sheet1",
+      { cache: "no-store" }
+    );
+
+    data = await res.json();
+  } catch (error) {
+    console.log("Fetch error:", error);
+  }
 
   return (
     <main style={{ padding: 25, fontFamily: "Arial" }}>
       <h1>My Links 🔥</h1>
+
+      {data.length === 0 && <p>No data found</p>}
 
       {data.map((item, i) => (
         <div key={i} style={{ margin: "10px 0" }}>
