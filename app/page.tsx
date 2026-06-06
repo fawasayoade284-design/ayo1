e42b3ce3be7f48f492939d17fc231c122512ffa0
fawasayoade284-design <fawasayoade284@gmail.com>
2026-06-06 08:27:57 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-export async function getServerSideProps() {
-  const res = await fetch("https://opensheet.elk.sh/1qwtUjm1ghhxuBK_urQZntHKaKKbH8eAeOT6m23__W0Y/Sheet1");
+
+export default async function Home() {
+  const res = await fetch("https://opensheet.elk.sh/1qwtUjm1ghhxuBK_urQZntHKaKKbH8eAeOT6m23__W0Y/Sheet1", {
+    cache: "no-store"
+  });
+
   const data = await res.json();
 
-  return { props: { data } };
-}
-
-export default function Home({ data }) {
   return (
-    <div style={{ padding: 25, fontFamily: "Arial" }}>
-      <h1>My Link Page 🔥</h1>
+    <div style={{ padding: 20, fontFamily: "Arial" }}>
+      <h1>My Links 🔥</h1>
 
       {data.map((item, i) => (
         <div key={i} style={{ margin: "10px 0" }}>
